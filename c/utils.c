7087b97e0de2f62d22622e613a0b4e420f311093
Bryan Waite <bryanwaite@gmail.com>
2015-06-01 18:23:40 -0400

    Missed utils files

---
+++ c/utils.c
@@ -0,0 +1,19 @@
+#include "utils.h"
+
+double min(double i, double j){
+        if(i < j)
+                return i;
+        return j;
+}
+
+double max(double i, double j){
+        if(i > j)
+                return i;
+        return j;
+}
+
+void lowercase_string(char *str){
+        size_t i;
+        for(i = 0; i < strlen(str); i++)
+                str[i] = tolower(str[i]);
+}
---
+++ c/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+double min(double i, double j);
+double max(double i, double j);
+void lowercase_string(char *str);
+#endif
